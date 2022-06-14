7bfe2148d2df0dd57a745a31ef0e20f8e5b2e274
Alexandr Eremeev <eremeev.a.e@mail.ru>
2022-06-14 14:59:55 +0700

    first commit

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import Multiselect from '@vueform/multiselect'
-
-const app = createApp(App);
-app.component('multiselect', Multiselect)
-
-app.mount("#app");
